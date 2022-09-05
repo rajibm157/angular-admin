86cd00f02d6b5cd5d85ad5b23b6db8059cf5f5b2
Rajib Mandal <rajib.mandal@sbr-technologies.com>
2022-09-05 21:28:12 +0530

    Layout update

--- src/app/core/layout/default/default.component.ts
+++ src/app/core/layout/default/default.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
   styleUrls: ['./default.component.scss']
 })
-export class DefaultComponent implements OnInit {
+export class DefaultComponent implements AfterViewInit {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.observer.observe(['(max-width:800px)']).subscribe((res: any) => {
+      if (res.matches) {
+        setTimeout(() => {
+          this.sidenav.mode = 'over';
+          this.sidenav.close()
+        }, 0);
+      } else {
+        this.sidenav.mode = 'side';
+      }
+    })
   }
 
 }
